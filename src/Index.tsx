6cf9ac4e1e4bc8d77c4750736f30e018a8eac7d3
emilsed <emily83_d@hotmail.com>
2025-01-30 19:26:17 -0300

    tarjeta tour

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {Component, Tours} from "./Component";
 import Product, {Navbar} from "./Product";
+import { Button } from "./Button";
+import { TaskCard } from "./Task";
+ReactDOM.createRoot(document.getElementById('root')as HTMLElement).render(
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-root.render (
 <>
+<TaskCard />
+{/*  <Button text= "Mas Informacion"/>
 <Component />
 <Tours />
 <Product />
-<Navbar />
+<Navbar />*/}
 </>
-);
+ );
---
+++ src/Task.tsx
@@ -0,0 +1,10 @@
+import './Styles.css';
+
+export function TaskCard(){
+    return (
+    <div className = 'card'>
+        <h1>Mi primer tours</h1>
+        <p>Tarea realizada</p>
+    </div>
+    );
+}
